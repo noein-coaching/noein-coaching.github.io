15ce2a61a7720bbeef950ebd94d7f3e6dff1abbf
noein-coaching <alex.anagnostopoulou@gmail.com>
2026-02-05 11:11:58 +0000

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,48 +3,47 @@ $(function() {
   "use strict";
 
   /*===============================================
-    Preloaders
-  ===============================================*/
-  var $body = $("body");
+  Preloaders
+===============================================*/
+var $body = $("body");
 
-  $(window).on("load", function () {
-    $body.addClass("loaded");
-  });
+$(window).on("load", function () {
+  $body.addClass("loaded");
+});
 
-  if ($body.attr("data-preloader") === "1") {
-    $body.append($("<div class='preloader preloader-1'><div><span></span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "2") {
-    $body.append($("<div class='preloader preloader-2'><div class='preloader-message'>A space to <i>think</i>.</div></div>"));
-
-  else if ($body.attr("data-preloader") === "3") {
-    $body.append($("<div class='preloader preloader-3'><div><span></span><span></span><span></span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "4") {
-    $body.append($("<div class='preloader preloader-4'><div><span></span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "5") {
-    $body.append($("<div class='preloader preloader-5'><div><span></span><span></span><span></span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "6") {
-    $body.append($("<div class='preloader preloader-6'><div><span></span><span></span><span></span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "7") {
-    $body.append($("<div class='preloader preloader-7'><div><span></span><span></span><span></span><span></span><span></span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "8") {
-    $body.append($("<div class='preloader preloader-8'><div><span></span><span></span><span></span><span></span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "9") {
-    $body.append($("<div class='preloader preloader-9'><div><span class='spinner-box'><span></span><span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "10") {
-    $body.append($("<div class='preloader preloader-10'><div><h6 class='heading-uppercase'>Loading</h6><span class='spinner'></span></div></div>"));
-  }
-  else if ($body.attr("data-preloader") === "11") {
-    $body.append($("<div class='preloader preloader-11'><div><span class='spinner'><span></div></div>"));
-  }
-
+if ($body.attr("data-preloader") === "1") {
+  $body.append($("<div class='preloader preloader-1'><div><span></span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "2") {
+  $body.append($("<div class='preloader preloader-2'><div class='preloader-message'>A space to <i>think</i>.</div></div>"));
+}
+else if ($body.attr("data-preloader") === "3") {
+  $body.append($("<div class='preloader preloader-3'><div><span></span><span></span><span></span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "4") {
+  $body.append($("<div class='preloader preloader-4'><div><span></span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "5") {
+  $body.append($("<div class='preloader preloader-5'><div><span></span><span></span><span></span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "6") {
+  $body.append($("<div class='preloader preloader-6'><div><span></span><span></span><span></span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "7") {
+  $body.append($("<div class='preloader preloader-7'><div><span></span><span></span><span></span><span></span><span></span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "8") {
+  $body.append($("<div class='preloader preloader-8'><div><span></span><span></span><span></span><span></span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "9") {
+  $body.append($("<div class='preloader preloader-9'><div><span class='spinner-box'><span></span><span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "10") {
+  $body.append($("<div class='preloader preloader-10'><div><h6 class='heading-uppercase'>Loading</h6><span class='spinner'></span></div></div>"));
+}
+else if ($body.attr("data-preloader") === "11") {
+  $body.append($("<div class='preloader preloader-11'><div><span class='spinner'><span></div></div>"));
+}
 
   /*===============================================
     Navbar Menu
